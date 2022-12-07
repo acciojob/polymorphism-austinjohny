662bc9ba11aed24b23bc8ef564d88ed9096c2482
Austin Johny <austinjohny9868@gmail.com>
2022-12-07 18:26:40 +0530

    Made all requested changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
-public class Main {
+class Product{
+    public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+    public double product(double x,double y){
+        return x*y;
+    }
 
+}
+
+public class Main {
+    public static void main(String[] args){
+        Product p = new Product();
+        int res1 = p.product(12,12);
+        System.out.println(res1);
+        int res2 = p.product(12,12,12);
+        System.out.println(res2);
+        double res3 = p.product(20.5,90.6);
+        System.out.println(res3);
+    }
 }
